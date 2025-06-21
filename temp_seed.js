350da6d87198cfa044c5d4500d1f9c81e8b66332
vicsnet <adesanmiadebisi@gmail.com>
2025-06-22 00:45:25 +0100

    code works

---
+++ temp_seed.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: '../.env' });
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+ 
+async function main() {
+// ... existing code ...
+} 
